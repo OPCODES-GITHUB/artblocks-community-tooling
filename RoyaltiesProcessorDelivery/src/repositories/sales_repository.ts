00c0e33b05605d1f3a9ef66102c3428b8411925a
Theo Bernardin <theo.bernardin@gmail.com>
2022-04-25 15:58:02 +0200

    changing request to fit the entities from the new subgraph supporting looksrare, refactoring name of the class and file

---
+++ RoyaltiesProcessorDelivery/src/repositories/sales_repository.ts
@@ -0,0 +1,57 @@
+import { gql } from "graphql-request"
+
+import { GraphQLDatasource } from "../datasources/graphQL_datasource";
+import { T_Sale } from "../types/graphQL_entities_def";
+
+const QUERY_GET_OPENSEA_SALES = gql`
+query getSales($first: Int!, $skip: Int!) {
+  sales(first: $first, skip: $skip, where: {WHERE_CLAUSE}, orderBy: blockNumber, orderDirection: desc) {
+    id
+    exchange
+    saleType
+    price
+    paymentToken
+    blockNumber
+    blockTimestamp
+    isPrivate
+    summaryTokensSold
+    saleLookupTables {
+      id
+      token {
+        id
+        contract {
+          id
+        }
+        project {
+          id
+          name
+          artistAddress
+          curationStatus
+          additionalPayee
+          additionalPayeePercentage
+        }
+      }
+    }
+  }
+}`;
+
+
+type T_QueryVariable_GetSales = {
+  first: number,
+  skip: number,
+}
+
+export class SalesRepository {
+  #graphQLDatasource: GraphQLDatasource;
+
+  constructor(graphQLDatasource: GraphQLDatasource) {
+    this.#graphQLDatasource = graphQLDatasource;
+  }
+
+  async getSalesBetweenBlockNumbers(variables: T_QueryVariable_GetSales, blockNumberGte: number, blockNumberLt: number): Promise<T_Sale[]> {
+    const query = QUERY_GET_OPENSEA_SALES.replace("WHERE_CLAUSE", `blockNumber_gte: ${blockNumberGte}, blockNumber_lt: ${blockNumberLt}`);
+
+    const resp = await this.#graphQLDatasource.query(query, variables);
+    return resp.openSeaSales as T_Sale[];
+  }
+}
